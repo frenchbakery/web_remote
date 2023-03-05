325ae0bd637a01e545b60a4f90ef43c2923a185e
nilusink <nilusink@protonmail.com>
2023-03-05 19:10:37 +0100

    enhanced rslider with a variable set "from" and "to" point.
    Took 58 minutes

--- js/index.js
+++ js/index.js
@@ -11,3 +11,9 @@ function toMotors()
 {
     window.location.href = "/web_remote/motors.html?_ijt=odml316v6bgqih95m0godgsvgp&_ij_reload=RELOAD_ON_SAVE";
 }
+
+
+function toServos()
+{
+    window.location.href = "/web_remote/servos.html?_ijt=odml316v6bgqih95m0godgsvgp&_ij_reload=RELOAD_ON_SAVE";
+}
